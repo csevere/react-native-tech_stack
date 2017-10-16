f40e2ae1a07b65043b5a99dd6545fd316ad34f53
csevere <carla.severe@gmail.com>
2017-10-15 20:35:17 -0400

    finished tech stack

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+    Text, 
+    TouchableWithoutFeedback, 
+    View, 
+    LayoutAnimation 
+} from 'react-native';
 import { connect } from 'react-redux'; //use connect to call action creator 
 import { CardSection } from './common';
 import  * as actions from '../actions'; //give me everything exported from actions file
@@ -7,13 +12,21 @@ import  * as actions from '../actions'; //give me everything exported from actio
 
 class ListItem extends Component{
 
+    componentWillUpdate(){
+        LayoutAnimation.spring();
+    }
+
     renderDescription(){
-        const { library, selectedLibraryId} = this.props; 
+        const { library, expanded} = this.props; 
+        const { textStyle } = styles;
 
-        if(library.id === selectedLibraryId){
+        if(expanded){
             return(
-                <Text>{library.description}</Text>
-
+                <CardSection>
+                    <Text style = {textStyle}>
+                        {library.description}
+                    </Text>
+                </CardSection> 
             );
         }
     }
@@ -48,11 +61,18 @@ const styles = {
         fontSize: 20,
         padding: 15,
         fontFamily: 'Copperplate'
+    },
+
+    textStyle:{
+        paddingLeft: 15,
+        flex: 1
+
     }
 }
 
-const mapStateToProps = state => {
-    return { selectedLibraryId: state.selectedLibraryId }
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id
+    return { expanded }
 
 };
 
@@ -60,3 +80,6 @@ export default connect(mapStateToProps, actions)(ListItem);
 
 //the first argument is for mapStateToProps, if you don't to pass mapStateToProps, replace it with null 
 //touchablewithoutfeedback - no highlight or fancy opacity fade, etc 
+//mapStateToProps hold two arguments > ownProps are the props that were passed to the component that we're wrapping, the listitem comp, when displayed by librarylist, it is passed a library, which is why we reference this.props.library; 
+//ownProps object is exactly equal to this.props inside the component; any props we pass into this listitem will show up in mapStateToProps as ownProps
+// expanded pulls some amt of logic out of the component
